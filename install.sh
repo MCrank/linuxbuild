a41ee105b51e3bf19d5507fb452a13782bc77ac6
Marco Crank <marco_mcse@hotmail.com>
2021-09-18 14:51:14 -0500

    Initial Push

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y ansible
+# sudo sh -c "echo 'localhost ansible_connection=local' > /etc/localhosts"
+
+exec ansible-playbook --ask-become-pass ./playbook.yml
